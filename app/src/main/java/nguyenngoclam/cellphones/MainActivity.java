0821a707f5481afd7d851b657461b29d37a3222c
LamNguyenUIT <ngoclam9999@gmail.com>
2015-06-01 01:46:36 +0700

    menu left

--- app/src/main/java/nguyenngoclam/cellphones/MainActivity.java
+++ app/src/main/java/nguyenngoclam/cellphones/MainActivity.java
@@ -60,12 +60,32 @@ public class MainActivity extends ActionBarActivity
         switch (number) {
             case 1:
                 mTitle = getString(R.string.title_section1);
+
                 break;
             case 2:
                 mTitle = getString(R.string.title_section2);
+
                 break;
             case 3:
                 mTitle = getString(R.string.title_section3);
+
+                break;
+            case 4:
+                mTitle = getString(R.string.title_section4);
+
+                break;
+
+            case 5:
+                mTitle = getString(R.string.title_section5);
+
+                break;
+            case 6:
+                mTitle = getString(R.string.title_section6);
+
+                break;
+            case 7:
+                mTitle = getString(R.string.title_section7);
+
                 break;
         }
     }
